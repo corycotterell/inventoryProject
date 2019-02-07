fcdcb85fc79b47b96f464a6432a3fba209163b3d
Cory Cotterell <cordawg123@gmail.com>
2019-02-06 22:58:33 -0700

    added backend routes for login and register

---
+++ api/app/Controllers/Http/UserController.js
@@ -0,0 +1,35 @@
+'use strict'
+const User = use('App/Models/User')
+
+class UserController {
+    async login({ request, auth, response }) {
+        
+        const email = request.input("email")
+        const password = request.input("password");
+        try {
+            if (await auth.attempt(email, password)) {
+                let user = await User.findBy('email', email)
+                let accessToken = await auth.generate(user)
+                return response.json({ "message": "success", "access_token": accessToken })
+            }
+
+        }
+        catch (e) {
+            return e
+        }
+    }
+    async register({ request, response }) {
+
+        const user = await User.create({
+
+            first_name: request.input("first_name"),
+            last_name: request.input("last_name"),
+            email: request.input("email"),
+            password: request.input("password"),
+        })
+        response.send(user)
+    }
+
+}
+
+module.exports = UserController
